09fe1240217bf04ee8709869039d87e62382bd74
MarwaAshraf1812 <marashraf090@gmail.com>
2024-08-19 19:07:10 +0300

    Refactoring the code to be more organized

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
 
-function dbConection() {
+async function dbConection() {
     try {
-        mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('Error connecting to MongoDB:', err);
